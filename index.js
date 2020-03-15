496e372243cbec88ef3c0625dc061fea065c232a
Igor Zelenov <igorg.zelenov@gmail.com>
2020-03-14 23:23:52 -0400

    new commit

--- index.js
+++ index.js
@@ -409,3 +409,24 @@ let priceRanges = [
 ];
 
 let restaurants = [{ averagePerPerson: 5 }];
+
+
+//------------------------------
+//------------------------------
+
+function isPrime(element, index, array) {
+  var start = 2;
+  while (start <= Math.sqrt(element)) {
+    if (element % start++ < 1) {
+      return false;
+    }
+  }
+  return element > 1;
+}
+
+console.log([4, 6, 8, 12].find(isPrime)); // undefined, не найдено
+console.log([4, 5, 8, 12].find(isPrime)); // 5
+
+//------------------------------
+
+
