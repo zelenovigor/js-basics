0463045fd00b3a7530916a18f651e357ea624e82
Igor Zelenov <igorg.zelenov@gmail.com>
2020-03-24 22:44:29 -0400

    spread opeartor

--- index.js
+++ index.js
@@ -527,3 +527,33 @@ var p = new Person();
 //------------------------------
 //------------------------------
 
+function applyAndNew(constructor, args) {
+  function partial () {
+     return constructor.apply(this, args);
+  };
+  if (typeof constructor.prototype === "object") {
+     partial.prototype = Object.create(constructor.prototype);
+  }
+  return partial;
+}
+
+
+function myConstructor () {
+  console.log("arguments.length: " + arguments.length);
+  console.log(arguments);
+  this.prop1="val1";
+  this.prop2="val2";
+};
+
+var myArguments = ["hi", "how", "are", "you", "mr", null];
+var myConstructorWithArguments = applyAndNew(myConstructor, myArguments);
+
+console.log(new myConstructorWithArguments);
+// (internal log of myConstructor):           arguments.length: 6
+// (internal log of myConstructor):           ["hi", "how", "are", "you", "mr", null]
+// (log of "new myConstructorWithArguments"): {prop1: "val1", prop2: "val2"}
+
+//------------------------------
+//------------------------------
+//------------------------------
+
