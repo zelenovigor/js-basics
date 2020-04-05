5b9ecceb210fa0401dc088c007f8046ab747f24d
Igor Zelenov <iz@Igors-MacBook-Pro.local>
2020-04-04 22:04:36 -0400

    filtering an array

--- index.js
+++ index.js
@@ -557,3 +557,42 @@ console.log(new myConstructorWithArguments);
 //------------------------------
 //------------------------------
 
+let arr = [
+  { id: 15 },
+  { id: -1 },
+  { id: 0 },
+  { id: 3 },
+  { id: 12.2 },
+  { },
+  { id: null },
+  { id: NaN },
+  { id: 'undefined' }
+]
+
+let invalidEntries = 0
+
+function isNumber(obj) {
+  return obj !== undefined && typeof(obj) === 'number' && !isNaN(obj)
+}
+
+function filterByID(item) {
+  if (isNumber(item.id) && item.id !== 0) {
+    return true
+  } 
+  invalidEntries++
+  return false;
+}
+
+let arrByID = arr.filter(filterByID)
+
+console.log('Filtered Array\n', arrByID)
+// Filtered Array
+// [{ id: 15 }, { id: -1 }, { id: 3 }, { id: 12.2 }]
+
+console.log('Number of Invalid Entries = ', invalidEntries)
+// Number of Invalid Entries = 5
+
+//------------------------------
+//------------------------------
+//------------------------------
+
