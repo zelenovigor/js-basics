f8793845306de1d0108a22c88e3841e88efbcd90
Igor Zelenov <igorg.zelenov@gmail.com>
2020-04-29 00:00:18 -0400

    spread

--- index.js
+++ index.js
@@ -1146,3 +1146,29 @@ console.log('The new color of my Honda is ' + myHonda.color)
 // Display the color of myHonda referenced from both arrays.
 console.log('myCar[0].color = ' + myCar[0].color)
 console.log('newCar[0].color = ' + newCar[0].color)
+
+function applyAndNew(constructor, args) {
+  function partial () {
+     return constructor.apply(this, args);
+  };
+  if (typeof constructor.prototype === "object") {
+     partial.prototype = Object.create(constructor.prototype);
+  }
+  return partial;
+}
+
+
+function myConstructor () {
+  console.log("arguments.length: " + arguments.length);
+  console.log(arguments);
+  this.prop1="val1";
+  this.prop2="val2";
+};
+
+const myArguments = ["hi", "how", "are", "you", "mr", null];
+const myConstructorWithArguments = applyAndNew(myConstructor, myArguments);
+
+console.log(new myConstructorWithArguments);
+//  (internal log of myConstructor):           arguments.length: 6
+//  (internal log of myConstructor):           ["hi", "how", "are", "you", "mr", null]
+//  (log of "new myConstructorWithArguments"): {prop1: "val1", prop2: "val2"}
